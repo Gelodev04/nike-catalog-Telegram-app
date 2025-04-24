086777661a43d1e162f0fd0dbf81f89e87415ebd
gelo <gelodev83@gmail.com>
2025-04-24 21:48:54 +0800

    updated

--- js/renderproducts.js
+++ js/renderproducts.js
@@ -1,26 +1,58 @@
 const products = [
-    { name: "Product Name Here", price: "$25.99", image: "/images/products/product1.jpg" },
-    { name: "Product Name Here", price: "$25.99", image: "/images/products/product2.jpg" },
-    { name: "Product Name Here", price: "$25.99", image: "/images/products/product3.jpg" },
-    { name: "Product Name Here", price: "$25.99", image: "/images/products/product4.jpg" },
-    { name: "Product Name Here", price: "$25.99", image: "/images/products/product5.jpg" },
-    { name: "Product Name Here", price: "$25.99", image: "/images/products/product6.jpg" },
-    { name: "Product Name Here", price: "$25.99", image: "/images/products/product7.jpg" },
-    { name: "Product Name Here", price: "$25.99", image: "/images/products/product8.jpg" },
-    // Add more...
-  ];
+  {
+    name: "Product Name Here",
+    price: "$25.99",
+    image: "/images/products/product1.jpg",
+  },
+  {
+    name: "Product Name Here",
+    price: "$25.99",
+    image: "/images/products/product2.jpg",
+  },
+  {
+    name: "Product Name Here",
+    price: "$25.99",
+    image: "/images/products/product3.jpg",
+  },
+  {
+    name: "Product Name Here",
+    price: "$25.99",
+    image: "/images/products/product4.jpg",
+  },
+  {
+    name: "Product Name Here",
+    price: "$25.99",
+    image: "/images/products/product5.jpg",
+  },
+  {
+    name: "Product Name Here",
+    price: "$25.99",
+    image: "/images/products/product6.jpg",
+  },
+  {
+    name: "Product Name Here",
+    price: "$25.99",
+    image: "/images/products/product7.jpg",
+  },
+  {
+    name: "Product Name Here",
+    price: "$25.99",
+    image: "/images/products/product8.jpg",
+  },
+  // Add more...
+];
 
-  const productList = document.getElementById('product-list');
+const productList = document.getElementById("product-list");
 
-  products.forEach(product => {
-    const li = document.createElement('li');
-    li.className = "bg-white rounded group cursor-pointer ";
+products.forEach((product) => {
+  const li = document.createElement("li");
+  li.className = "bg-white rounded group cursor-pointer ";
 
-    li.innerHTML = `
-      <div class="overflow-hidden rounded-md  "><img src="${product.image}" alt="${product.name}" class="w-full h-full mb-1 group-hover:scale-[1.1] duration-100 ease-out " /></div>
+  li.innerHTML = `
+      <div class="overflow-hidden rounded-md  mb-1"><img src="${product.image}" alt="${product.name}" class="w-full  rounded-md  group-hover:scale-[1.1] duration-100 ease-out " /></div>
       <h3 class="text-[13.5px] font-semibold underline ">${product.name}</h3>
       <p class="text-[12px] text-[#4A9E14]"><span>Starting from: </span>${product.price}</p>
     `;
 
-    productList.appendChild(li);
-  });
+  productList.appendChild(li);
+});
